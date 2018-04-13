5944232f5fa3d4d08ae408f08e1ec1a441bbcdf0
clour <sonesr@hotmail.com>
2018-04-13 13:20:46 +0800

    Remove RestController import!

--- app/wede/src/main/java/org/clour/wede/StartApp.java
+++ app/wede/src/main/java/org/clour/wede/StartApp.java
@@ -3,12 +3,9 @@ package org.clour.wede;
 import org.clour.mode.Demodule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 //@RestController
